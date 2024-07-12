7c124c067b0c7bd5ae54b1484a3189a1d99fcc25
aidankeaveny <aidan.keaveny@gmail.com>
2024-07-12 12:59:35 -0400

    new files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO BOTTLE (address, number_of_bottles, picked_up) VALUES ('123 Main St', 10, false);
+INSERT INTO BOTTLE (address, number_of_bottles, picked_up) VALUES ('456 Elm St', 15, false);
