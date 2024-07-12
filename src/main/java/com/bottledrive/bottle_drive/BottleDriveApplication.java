a2762d7556fe71df04fc8396dfc44a7a14e6ce8a
aidankeaveny <aidan.keaveny@gmail.com>
2024-07-12 10:55:14 -0400

    first

---
+++ src/main/java/com/bottledrive/bottle_drive/BottleDriveApplication.java
@@ -0,0 +1,13 @@
+package com.bottledrive.bottle_drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottleDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BottleDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bottledrive/bottle_drive/BottleDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bottledrive.bottle_drive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BottleDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
